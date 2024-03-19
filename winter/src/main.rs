df9aa05e000dfca8b7bfdc3e9d34de8cdaa01f36
negative-seven <21268649+negative-seven@users.noreply.github.com>
2024-03-19 23:27:50 +0100

    Add rudimentary movie support

--- winter/src/main.rs
+++ winter/src/main.rs
@@ -10,6 +10,7 @@ fn main() -> Result<()> {
 
     let mut arguments = env::args().skip(1);
     let executable_path = arguments.next().expect("missing argument: executable path");
+    let movie_path = arguments.next();
 
     let mut runtime = Runtime::new(
         executable_path,
@@ -20,8 +21,30 @@ fn main() -> Result<()> {
         }),
     )?;
     runtime.resume()?;
-    loop {
-        runtime.advance_time(Duration::from_secs_f64(1.0 / 60.0))?;
-        runtime.wait_until_idle()?;
+    if let Some(movie_path) = movie_path {
+        for line in std::fs::read_to_string(movie_path)?.lines() {
+            let mut tokens = line.split_ascii_whitespace();
+            match tokens.next().unwrap() {
+                "Key" => {
+                    let key_id = tokens.next().unwrap().parse::<u8>()?;
+                    let key_state = tokens.next().unwrap().parse::<u8>()? != 0;
+                    runtime.set_key_state(key_id, key_state)?;
+                }
+                "Wait" => {
+                    let secs = tokens.next().unwrap().parse::<f64>()?;
+                    runtime.advance_time(Duration::from_secs_f64(secs))?;
+                    // std::thread::sleep(Duration::from_secs_f64(0.1));
+                    runtime.wait_until_idle()?;
+                }
+                _ => unimplemented!(),
+            }
+        }
+    } else {
+        loop {
+            runtime.advance_time(Duration::from_secs_f64(1.0 / 60.0))?;
+            runtime.wait_until_idle()?;
+        }
     }
+
+    Ok(())
 }
